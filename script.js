8de04db60f50f02c08fc6fc043fd586055579514
Yilikal250 <teshomeyilikal250@gmail.com>
2024-06-29 22:46:10 +0200

    animation and introduction added

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ document.getElementById("hamburger").addEventListener("click", function() {
     toggleList.classList.toggle("hidden");
     toggleList.classList.toggle("visible");
 });
+
+const textArray = ["Full Stack Developer", "HTML", "CSS", "JavaScript", "React", "Python", "Django", "SQL"];
+const typingTextElement = document.getElementById('typing-text');
+let textArrayIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeWriter() {
+    const currentText = textArray[textArrayIndex];
+    
+    if (isDeleting) {
+        typingTextElement.textContent = currentText.substring(0, charIndex--);
+    } else {
+        typingTextElement.textContent = currentText.substring(0, charIndex++);
+    }
+
+    if (!isDeleting && charIndex === currentText.length) {
+        setTimeout(() => isDeleting = true, 2000);  // Pause before start deleting
+    } else if (isDeleting && charIndex === 0) {
+        isDeleting = false;
+        textArrayIndex = (textArrayIndex + 1) % textArray.length;
+    }
+
+    const delay = isDeleting ? 50 : 100;
+    setTimeout(typeWriter, delay);
+}
+
+document.addEventListener("DOMContentLoaded", () => setTimeout(typeWriter, 1000));
